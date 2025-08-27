5e62e7f4185e10551098538906490f18d425bce1
Jav <javarruedas@gmail.com>
2025-08-27 12:29:11 +0800

    add captain

---
+++ aspnetapp/Classes/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace aspnetapp.Classes
+{
+    public class Person
+    {
+        string name;
+        string position;
+
+        public string Name { get => name; set => name = value; }
+        public string Position { get => position; set => position = value; }
+    }
+}
